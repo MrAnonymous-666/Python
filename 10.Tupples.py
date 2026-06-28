482a787c4bc373122626db93b1ad331c6a5925dc
MrAnonymous-666 <kartikpal20062004@gmail.com>
2026-06-29 04:48:52 +0530

    .

---
+++ 10.Tupples.py
@@ -0,0 +1,12 @@
+# Unpacking feature of python
+
+coordinates = (1,2,3)
+
+x,y,z = coordinates
+# x = coordinates [0]
+# y = coordinates [1]
+# z = coordinates [2]
+
+print(y) 
+
+# it is useful method for both Tupples and List 
---
+++ 11.Dictionaries.py
@@ -0,0 +1,8 @@
+customer = {
+    "name" : "John Smith",
+    "age" :  30,
+    "is_verified" : True
+
+}
+print(customer["name"]) # OR
+print(customer.get("name")) 
